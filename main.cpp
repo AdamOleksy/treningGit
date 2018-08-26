b0181897f55344444c6078ccd96f075b1589c4fe
Adam Oleksy <adamoleksy1983@gmail.com>
2018-08-26 19:51:11 +0200

    Commit do cos_nowego

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main(){
     std::cout << "Hello" << std::endl;
-    std::cout << "Kolejna linijka";
+    std::cout << "Kolejna linijka" << std::endl;
+    std::cout << "To jest galaz cos_nowego" << std::endl;
     return 0;
 }
